b1db9ff58766f55f36591923c6776760d18e766c
teng wang <wt0318@outlook.com>
2021-12-03 14:28:44 +0800

    Add files via upload

---
+++ scripts/model.py
@@ -0,0 +1,23 @@
+from transformers import AutoModel, AutoTokenizer, AdamW
+import torch.nn as nn
+import torch
+import numpy as np
+
+from config import set_seed, CONFIG
+
+set_seed(CONFIG.seed)
+
+
+class Model(nn.Module):
+    def __init__(self, model_name):
+        super(Model, self).__init__()
+        self.model = AutoModel.from_pretrained(model_name)
+        self.drop = nn.Dropout(p=0.2)
+        self.fc = nn.Linear(768, CONFIG.n_classes)
+
+    def forward(self, ids, mask):
+        out = self.model(input_ids=ids, attention_mask=mask, output_hidden_states=False)
+        out = self.drop(out[1])
+        outputs = self.fc(out)
+        return outputs
+
---
+++ scripts/train.py
@@ -0,0 +1,13 @@
+from config import CONFIG
+from model import Model
+from transformers import AdamW
+from learner import fetch_scheduler, run_training
+
+for fold in range(0, CONFIG.n_fold):
+    print(f"__________fold {fold}________________")
+    model = Model(CONFIG.model_name)
+    model.to(CONFIG.device)
+    optimizer = AdamW(model.parameters(), lr=CONFIG.learning_rate, weight_decay=CONFIG.weight_decay)
+    scheduler = fetch_scheduler(optimizer)
+
+    model, history = run_training(model, optimizer, scheduler, num_epochs=CONFIG.epochs, fold=fold)
